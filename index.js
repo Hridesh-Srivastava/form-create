3fa0a43258ed865900cf12ba419d93531a48fb64
Hridesh <hrideshsrivastavaa@outlook.com>
2024-05-26 15:43:22 +0530

    pushed

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(cors(corsConfig));
-app.use(cors(corsConfig));
+app.use(cors());
 
 
 const User = mongoose.model('User', {
